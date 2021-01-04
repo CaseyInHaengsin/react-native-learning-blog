c6ead65e702c3a68f28d1569de47fd3cb6858fac
CaseyInHaengsin <casey.rowley@kuali.co>
2021-01-03 20:03:00 -0700

    added bog post icon and onpress listener.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 
 const IndexScreen = () => {
@@ -18,7 +19,12 @@ const IndexScreen = () => {
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>
+                    return <View style={styles.row}>
+                                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                                <TouchableOpacity onPress={() => console.log(item.id)}>
+                                    <Feather style={styles.icon} name="trash"/>
+                                </TouchableOpacity>
+                            </View>
                 }}
             />
 
@@ -27,6 +33,23 @@ const IndexScreen = () => {
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderBottomWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+    
+
+});
 
 export default IndexScreen;
